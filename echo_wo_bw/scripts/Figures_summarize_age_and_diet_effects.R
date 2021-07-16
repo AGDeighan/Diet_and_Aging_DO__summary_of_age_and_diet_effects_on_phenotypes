85666aef3d83995e7ce0729259270615664b6921
AGDeighan <andrew.g.deighan@gmail.com>
2021-07-16 17:56:02 -0400

    echo analysis without bodyweight as a covariate

---
+++ echo_wo_bw/scripts/Figures_summarize_age_and_diet_effects.R
@@ -0,0 +1,462 @@
+# 2021-07-06
+################################################################################
+#
+#   This script creates a figure that summarizes the diet and age effects for
+#   all the phenotypes
+#
+#
+#   Author:  Andrew Deighan
+#   E-mail: andrew.deighan@jax.org
+#
+################################################################################
+
+
+################################################################################
+## libraries etc #########################################################
+options(na.action = 'na.exclude')
+options(stringsAsFactors = FALSE)
+library(tidyverse)
+library(cowplot)
+DIET_COLORS <- c("seashell4", "skyblue", "royalblue4", "orange", "firebrick")
+
+################################################################################
+## helper functions #########################################################
+
+#####
+
+
+################################################################################
+## load dataset #########################################################
+
+DATASET <- readRDS('data/Echo_diet_age_dataset.rds')
+
+#####
+
+
+################################################################################
+##### Diet effects by TP #####
+
+PLOT_DATA <- DATASET$Diet_Effects %>% 
+  group_by(Timepoint) %>% 
+  mutate(
+    FWER = p.adjust(Diet_FTestPValue, 'holm')
+  ) %>% 
+  ungroup() %>% 
+  select(
+    Phenotype, Timepoint, FWER,
+    AL_Mean:`40_SE`
+  ) %>% 
+  pivot_longer(
+    cols = AL_Mean:`40_SE`,
+    names_to = 'K',
+    values_to = 'V'
+  ) %>% 
+  separate(
+    K,
+    into = c('Diet', 'K'),
+    sep = '_'
+  ) %>% 
+  pivot_wider(
+    names_from = 'K',
+    values_from = 'V'
+  ) %>% 
+  rowwise() %>% 
+  mutate(
+    Timepoint = factor(
+      Timepoint,
+      levels = DATASET$Timepoints
+    ),
+    Diet = factor(
+      Diet,
+      levels = c('AL', '1D', '2D', '20', '40')
+    ),
+    Position = as.numeric(Phenotype) + ((as.numeric(Diet) - 1)/4 - 0.5)*1/3,
+    Mean = (
+      (Mean - DATASET$Info_Table$Mean[DATASET$Info_Table$Phenotype == Phenotype]) /
+        DATASET$Info_Table$SD[DATASET$Info_Table$Phenotype == Phenotype]
+    ),
+    SE = (
+      SE/DATASET$Info_Table$SD[DATASET$Info_Table$Phenotype == Phenotype]
+    )
+  ) %>% 
+  ungroup()
+
+LIMITS <- PLOT_DATA %>% 
+  summarise(
+    M = max(abs(c(min(Mean - SE), max(Mean + SE))))
+  ) %>% 
+  unlist() %>% 
+  as.numeric()
+LIMITS <- ceiling((LIMITS + 0.1)/0.05)*0.05
+LIMITS <- c(-1*LIMITS, LIMITS)
+
+DIET_BY_TP_PLOT <- PLOT_DATA %>% 
+  ggplot() +
+  theme_minimal(
+    base_size = 9
+  ) +
+  geom_rect(
+    data = PLOT_DATA %>% 
+      group_by(Phenotype, Timepoint) %>% 
+      summarise() %>% 
+      ungroup() %>% 
+      mutate(
+        Position = as.numeric(Phenotype)
+      ) %>% 
+      filter(
+        Position %% 2 < 0.5
+      ) %>% 
+      mutate(
+        XMin = Position - 0.5,
+        XMax = Position + 0.5,
+        YMin = LIMITS[1],
+        YMax = LIMITS[2]
+      ),
+    aes(
+      xmin = XMin, xmax = XMax,
+      ymin = YMin, ymax = YMax
+    ),
+    fill = 'gray90',
+    alpha = 1/3
+  ) +
+  geom_hline(
+    yintercept = 0,
+    color = 'gray60',
+    linetype = 3
+  ) +
+  geom_point(
+    data = PLOT_DATA %>% 
+      group_by(
+        Phenotype, Timepoint, FWER
+      ) %>% 
+      summarise() %>% 
+      ungroup() %>% 
+      filter(
+        FWER < 0.05
+      ) %>% 
+      mutate(
+        X_Position = as.numeric(Phenotype),
+        Y_Position = LIMITS[2] - 0.05
+      ),
+    aes(x = X_Position, y = Y_Position),
+    shape = '*',
+    size = 5
+  ) +
+  geom_errorbar(
+    aes(x = Position, ymin = Mean - SE, ymax = Mean + SE, color = Diet),
+    size = 1/3.3,
+    width = 1/10
+  ) +
+  geom_point(
+    aes(x = Position, y = Mean, color = Diet),
+    size = 0.75
+  ) +
+  scale_y_continuous(
+    breaks = seq(-10, 10, 0.2),
+    minor_breaks = seq(-10, 10, 0.05),
+    limits = LIMITS,
+    exp = c(0, 0)
+  ) +
+  scale_x_continuous(
+    breaks = unique(as.numeric(PLOT_DATA$Phenotype)),
+    minor_breaks = seq(0, 500, 0.5),
+    labels = levels(PLOT_DATA$Phenotype),
+    limits = c(0.49, max(as.numeric(PLOT_DATA$Phenotype)) + 0.51),
+    expand = c(0, 0)
+  ) +
+  scale_color_manual(
+    values = DIET_COLORS,
+    na.value = 'black'
+  ) +
+  facet_grid(
+    Timepoint ~ .
+  ) +
+  theme(
+    axis.text.x = element_text(
+      angle = 45, vjust = 1, hjust=1
+    ),
+    panel.grid.major.x = element_blank(),
+    plot.caption = element_text(hjust = 0),
+    legend.position = 'none'
+  ) +
+  labs(
+    title = 'Diet effect on trait values by timepoint',
+    x = NULL,
+    y = 'Diet coefficient',
+    caption = paste0(
+      'The diet coefficient for each phenotype at each timepoint indicates the estimated effect in units of standard deviations',
+      '\n',
+      'The bars around the coefficient show +/- 1 standard error of the coefficient',
+      '\n',
+      'The asterisks indicate phenotypes for which the diet effect is significant at a FWER of 0.05',
+      '\n'
+    )
+  )
+
+rm(
+  PLOT_DATA, LIMITS
+)
+
+DIET_TP_WIDTH <- 6 + (length(DATASET$Info_Table$Phenotype) - 1)*0.2
+DIET_TP_HEIGHT <- 3 + (length(DATASET$Timepoints) - 1)*2
+
+
+#####
+
+
+################################################################################
+##### Age effects  #####
+
+PLOT_DATA <- rbind(
+  DATASET$Age_Effects %>% 
+    mutate(
+      Coef = Age_Slope/DATASET$Info_Table$SD[DATASET$Info_Table$Phenotype == Phenotype],
+      SE = Age_SE/DATASET$Info_Table$SD[DATASET$Info_Table$Phenotype == Phenotype],
+      FWER = p.adjust(Age_FTestPValue, 'holm'),
+      Phenotype = fct_rev(Phenotype),
+      Position = as.numeric(Phenotype),
+      Diet = NA,
+      Group = 'Overall'
+    ) %>% 
+    select(
+      Phenotype, Group, Diet, Position, Coef, SE, FWER
+    ),
+  DATASET$DietAge_Effects %>% 
+    mutate(
+      FWER = p.adjust(DietAge_FTestPValue, 'holm'),
+      Phenotype = fct_rev(Phenotype)
+    ) %>% 
+    select(
+      Phenotype,
+      AL_Slope:`40_SE`,
+      FWER
+    ) %>% 
+    pivot_longer(
+      cols = AL_Slope:`40_SE`,
+      values_to = 'V',
+      names_to = 'K'
+    ) %>% 
+    separate(
+      K,
+      into = c('Diet', 'K'),
+      sep = '_'
+    ) %>% 
+    pivot_wider(
+      values_from = 'V',
+      names_from = 'K'
+    ) %>% 
+    rowwise() %>% 
+    mutate(
+      Coef = Slope/DATASET$Info_Table$SD[DATASET$Info_Table$Phenotype == Phenotype],
+      SE = SE/DATASET$Info_Table$SD[DATASET$Info_Table$Phenotype == Phenotype],
+      Diet = factor(Diet, levels = rev(levels(DATASET$Data$Diet))),
+      Position = as.numeric(Phenotype) + ((as.numeric(Diet) - 1)/4 - 0.5)*1/3,
+      Group = 'Diet specific'
+    ) %>% 
+    ungroup() %>% 
+    select(
+      Phenotype, Group, Diet, Position, Coef, SE, FWER
+    )
+) %>% 
+  mutate(
+    Diet = factor(Diet, levels = levels(DATASET$Data$Diet)),
+    Group = factor(Group, levels = c('Overall', 'Diet specific'))
+  )
+  
+  
+
+
+LIMITS <- PLOT_DATA %>% 
+  summarise(
+    M = max(abs(c(min(Coef - SE), max(Coef + SE))))
+  ) %>% 
+  unlist() %>% 
+  as.numeric()
+LIMITS <- ceiling((LIMITS + 0.01)/0.05)*0.05
+LIMITS <- c(-1*LIMITS, LIMITS)
+
+
+AGE_EFFECTS_PLOT <- PLOT_DATA %>% 
+  ggplot() +
+  theme_minimal(
+    base_size = 9
+  ) +
+  geom_rect(
+    data = PLOT_DATA %>% 
+      group_by(Phenotype, Group) %>% 
+      summarise() %>% 
+      ungroup() %>% 
+      mutate(
+        Position = as.numeric(Phenotype)
+      ) %>% 
+      filter(
+        Position %% 2 < 0.5
+      ) %>% 
+      mutate(
+        YMin = Position - 0.5,
+        YMax = Position + 0.5,
+        XMin = LIMITS[1],
+        XMax = LIMITS[2]
+      ),
+    aes(
+      xmin = XMin, xmax = XMax,
+      ymin = YMin, ymax = YMax
+    ),
+    fill = 'gray90',
+    alpha = 1/3
+  ) +
+  geom_vline(
+    xintercept = 0,
+    color = 'gray60',
+    linetype = 3
+  ) +
+  geom_point(
+    data = PLOT_DATA %>% 
+      group_by(
+        Phenotype, Group, FWER
+      ) %>% 
+      summarise() %>% 
+      ungroup() %>% 
+      filter(
+        FWER < 0.05
+      ) %>% 
+      mutate(
+        Position = as.numeric(Phenotype),
+        X_Position = ifelse(
+          Group == 'Overall', LIMITS[1] + 0.005,
+          LIMITS[2] - 0.005
+        )
+      ),
+    aes(x = X_Position, y = Position),
+    shape = '*',
+    size = 5
+  ) +
+  geom_errorbarh(
+    aes(xmin = Coef - SE, xmax = Coef + SE, y = Position, color = Diet),
+    size = 1/3,
+    height = 1/10
+  ) +
+  geom_point(
+    aes(x = Coef, y = Position, color = Diet),
+    size = 0.8
+  ) +
+  scale_x_continuous(
+    breaks = seq(-10, 10, 0.05),
+    minor_breaks = seq(-10, 10, 0.01),
+    limits = LIMITS,
+    expand = c(0, 0)
+  ) +
+  scale_y_continuous(
+    breaks = unique(as.numeric(PLOT_DATA$Phenotype)),
+    minor_breaks = seq(0, 500, 0.5),
+    labels = rev(levels(PLOT_DATA$Phenotype)),
+    limits = c(0.49, max(as.numeric(PLOT_DATA$Phenotype)) + 0.51),
+    expand = c(0, 0)
+  ) +
+  scale_color_manual(
+    values = DIET_COLORS,
+    na.value = 'black'
+  ) +
+  facet_grid(
+    . ~ Group
+  ) +
+  theme(
+    panel.grid.major.y = element_blank(),
+    plot.caption = element_text(hjust = 0),
+    legend.position = 'none'
+  ) +
+  labs(
+    title = 'Age effect on trait values',
+    x = 'Age coefficient',
+    y = NULL,
+    caption = paste0(
+      'The age coefficient for each phenotype indicates the estimated change with age in units of standard deviations per month',
+      '\n',
+      'The bars around the coefficient show +/- 1 standard error of the coefficient',
+      '\n',
+      'The asterisks indicate phenotypes for which the direct age effect is significant (left) or the age-diet interaction is signficant (right) at a FWER of 0.05'
+    )
+  )
+
+rm(
+  PLOT_DATA, LIMITS
+)
+
+#####
+
+
+################################################################################
+##### Final plot  #####
+
+LEGEND <- get_legend(
+  DATASET$Data %>% 
+    ggplot() +
+    theme_minimal() +
+    geom_point(
+      aes(x = Timepoint, y = Age, color = Diet),
+      shape = 15, size = 4
+    ) +
+    scale_color_manual(
+      values = DIET_COLORS,
+      labels = c(
+        'Ad libitum  ',
+        '1-day fast  ',
+        '2-day fast  ',
+        '20% caloric restriction  ',
+        '40% caloric restriction  '
+      )
+    ) +
+    theme(
+      legend.position = 'top'
+    ) +
+    labs(
+      color = NULL
+    )
+)
+
+
+DIET_BY_TP_PLOT <- plot_grid(
+  LEGEND,
+  DIET_BY_TP_PLOT,
+  ncol = 1,
+  rel_heights = c(0.1, 1)
+)
+
+PLOT <- plot_grid(
+  DIET_BY_TP_PLOT,
+  AGE_EFFECTS_PLOT,
+  ncol = 1,
+  rel_heights = c(DIET_TP_HEIGHT, DIET_TP_WIDTH)
+)
+
+
+
+
+pdf(
+  paste0(
+    'figures/', DATASET$Assay,
+    '_diet_and_age_effects_summarized.pdf'
+  ),
+  height = DIET_TP_WIDTH + DIET_TP_HEIGHT,
+  width = DIET_TP_WIDTH
+)
+plot(PLOT)
+dev.off()
+
+#####
+
+
+################################################################################
+#####  #####
+
+#####
+
+
+################################################################################
+## clear workspace ###########################################
+
+rm(list = ls())
+pacman::p_unload('all')
+graphics.off()
+
+#####
---
+++ echo_wo_bw/scripts/echo_setup.R
@@ -0,0 +1,447 @@
+# 2021-07-15
+################################################################################
+#
+#   This script creates a phenotype information table to be used for estimating
+#   diet and age effects on the FACS phenotypes
+#
+#
+#   Author:  Andrew Deighan
+#   E-mail: andrew.deighan@jax.org
+#
+################################################################################
+
+
+################################################################################
+## libraries etc #########################################################
+options(na.action = 'na.exclude')
+options(stringsAsFactors = FALSE)
+library(tidyverse)
+
+################################################################################
+## helper functions #########################################################
+
+standardise <- function(x){
+  return(
+    (x - mean(x, na.rm = TRUE))/sd(x, na.rm = TRUE)
+  )
+}
+
+#####
+
+
+################################################################################
+## load data etc #########################################################
+
+ANIMAL_DATA <- read_csv(
+  'data/AnimalData_Processed_20210120.csv'
+) %>% 
+  mutate(
+    Diet = factor(
+      Diet,
+      levels = c('AL', '1D', '2D', '20', '40')
+    )
+  )
+
+PHENO_DATA <- read_csv(
+  'data/Echo_Processed_Raw_20201123.csv'
+) %>% 
+  mutate(
+    DateCollect = as.character(DateCollect),
+    AgeInDays = AgeInDays/30.4,
+    BW = ifelse(
+      is.na(BW_Weekly), BW_Test,
+      BW_Weekly
+    ),
+    BW = standardise(BW)
+  ) %>% 
+  rename(
+    Age = AgeInDays
+  )
+
+#####
+
+
+################################################################################
+## Specify assay #########################################################
+
+ASSAY <- 'Echo'
+
+#####
+
+
+################################################################################
+##### Select timepoints to test for diet and diet-age effects #####
+
+# The 34-month timepoint is imbalanced in terms of diet
+
+PHENO_DATA %>% 
+  left_join(
+    ANIMAL_DATA,
+    by = 'MouseID'
+  ) %>% 
+  select(
+    Timepoint, Diet
+  ) %>% 
+  table(useNA = 'ifany')
+#            Diet
+# Timepoint    AL  1D  2D  20  40
+#   10 months 182 174 180 184 176
+#   22 months 123 134 136 137 156
+#   34 months  17  34  41  56  91
+
+TIMEPOINTS_FOR_ANALYSIS <- paste0(c('10', '22'), ' months')
+
+#####
+
+
+################################################################################
+##### Set up info table #####
+
+# Exclude: None, I don't know enough about echo data to confidently say which
+# phenotypes are redundant or superfluous 
+
+PHENO_NAMES <- c(
+  'BPM',
+  'CardiacOutput',
+  'StrokeVolume',
+  'EF',
+  'FS',
+  'LVMass',
+  'LVMassCorrected',
+  'LVVol_dia',
+  'LVVol_sys',
+  'LVID_dia',
+  'LVID_sys',
+  'LVPW_dia',
+  'LVPW_sys',
+  'IVS_dia',
+  'IVS_sys'
+)
+
+PHENO_LEVELS <- c(
+  'BPM',
+  'CardiacOutput',
+  'StrokeVolume',
+  'EF',
+  'FS',
+  'LVMass',
+  'LVMassCorrected',
+  'LVVol_dia',
+  'LVVol_sys',
+  'LVID_dia',
+  'LVID_sys',
+  'LVPW_dia',
+  'LVPW_sys',
+  'IVS_dia',
+  'IVS_sys'
+)
+
+DESCRIPTIONS <- tibble(
+  Phenotype = PHENO_NAMES,
+  Description = c(
+    'heart rate (beats per minute)',
+    'cardiac output (micro liters per minute): BPM * StrokeVolume	',
+    'left ventricle stroke volume (micro liters): LVVol_dia - LVVol_sys',
+    'ejection fraction (%): (LVVol_dia - LVVol_sys) / LVVol_dia * 100%',
+    'fractional shortening (%): (LVID_dia - LVID_sys) / LVID_dia * 100%',
+    'left ventricle mass (mg): 1.053 * ((LVID_dia + LVPW_dia + IVS_dia)^3 - (LVID_dia^3))',
+    'corrected left ventricle mass (mg): 0.8 * LVMass',
+    'diastolic left ventricle volume (micro liters): 7/(2.4 + LVID_dia) * (LVID_dia^3)',
+    'systolic left ventricle volume (micro liters): 7/(2.4 + LVID_sys) * (LVID_sys^3)',
+    'diastolic left ventricular internal diameter (mm)',
+    'systolic left ventricular internal diameter (mm)',
+    'diastolic left ventricular posterior wall thickness (mm)',
+    'systolic left ventricular posterior wall thickness (mm)',
+    'diastolic inter-ventricular septum thickness (mm)',
+    'systolic inter-ventricular septum thickness (mm)'
+  )
+)
+
+
+TP_COUNT <- PHENO_DATA %>% 
+  select(
+    MouseID, Timepoint,
+    all_of(PHENO_NAMES)
+  ) %>% 
+  pivot_longer(
+    cols = all_of(PHENO_NAMES),
+    names_to = 'Phenotype',
+    values_to = 'Value'
+  ) %>% 
+  group_by(
+    Phenotype, Timepoint
+  ) %>% 
+  summarise(
+    N = sum(!is.na(Value))
+  ) %>% 
+  ungroup() %>% 
+  mutate(
+    Timepoint = paste0('NAll_', gsub(' ', '', Timepoint))
+  ) %>% 
+  pivot_wider(
+    names_from = 'Timepoint',
+    values_from = 'N'
+  )
+
+
+TP_DIET_COUNT <- ANIMAL_DATA %>% 
+  select(
+    MouseID, Diet
+  ) %>% 
+  left_join(
+    PHENO_DATA %>% 
+      select(
+        MouseID, Timepoint,
+        all_of(PHENO_NAMES)
+      ),
+    by = 'MouseID'
+  ) %>% 
+  pivot_longer(
+    cols = all_of(PHENO_NAMES),
+    names_to = 'Phenotype',
+    values_to = 'Value'
+  ) %>% 
+  filter(
+    !is.na(Timepoint)
+  ) %>% 
+  group_by(
+    Phenotype, Diet, Timepoint
+  ) %>% 
+  summarise(
+    N = sum(!is.na(Value))
+  ) %>% 
+  ungroup() %>% 
+  mutate(
+    Timepoint = paste0('N', as.character(Diet), '_', gsub(' ', '', Timepoint))
+  ) %>% 
+  select(
+    -Diet
+  ) %>% 
+  pivot_wider(
+    names_from = 'Timepoint',
+    values_from = 'N'
+  )
+
+
+SMRY_STATS <- PHENO_DATA %>% 
+  select(
+    MouseID, all_of(PHENO_NAMES)
+  ) %>% 
+  pivot_longer(
+    cols = all_of(PHENO_NAMES),
+    names_to = 'Phenotype',
+    values_to = 'Value'
+  ) %>% 
+  group_by(
+    Phenotype
+  ) %>% 
+  mutate(
+    Skewness = e1071::skewness(Value, na.rm = TRUE),
+    Kurtosis = e1071::kurtosis(Value, na.rm = TRUE),
+    Transform = 'none',
+    Value = ifelse(
+      Transform == 'none',
+      Value,
+      NA
+    )
+  ) %>% 
+  summarise(
+    Skewness = unique(Skewness),
+    Kurtosis = unique(Kurtosis),
+    Transform = unique(Transform),
+    SD = sd(Value, na.rm = TRUE),
+    Mean = mean(Value, na.rm = TRUE),
+    Median = median(Value, na.rm = TRUE),
+    Min = min(Value, na.rm = TRUE),
+    Max = max(Value, na.rm = TRUE)
+  )
+
+
+COVARIATES <- tibble(
+  Phenotype = PHENO_NAMES,
+  REF_Cov = c('MouseID:DateCollect'),
+  FEF_Cov = NA
+)
+
+
+PHENO_INFO_TABLE <- DESCRIPTIONS %>% 
+  left_join(
+    TP_COUNT,
+    by = 'Phenotype'
+  ) %>% 
+  left_join(
+    TP_DIET_COUNT,
+    by = 'Phenotype'
+  ) %>% 
+  left_join(
+    SMRY_STATS,
+    by = 'Phenotype'
+  ) %>% 
+  left_join(
+    COVARIATES,
+    by = 'Phenotype'
+  ) %>% 
+  mutate(
+    Assay = ASSAY,
+    Phenotype = factor(Phenotype, levels = PHENO_LEVELS)
+  ) %>% 
+  select(
+    Assay, Phenotype,
+    everything()
+  ) %>% 
+  arrange(
+    Phenotype
+  )
+  
+rm(DESCRIPTIONS, TP_COUNT, TP_DIET_COUNT, SMRY_STATS, COVARIATES, PHENO_NAMES, PHENO_LEVELS)
+
+
+
+#####
+
+
+################################################################################
+##### List of transformation function #####
+
+TRANSFORM_FUNCTIONS <- list(
+  NA
+)
+
+#####
+
+
+################################################################################
+##### Plot histograms of phenotypes #####
+
+PLOT_DATA <- PHENO_DATA %>% 
+  filter(
+    Timepoint %in% TIMEPOINTS_FOR_ANALYSIS
+  )
+
+pdf(
+  paste0(
+    'figures/', ASSAY,
+    '_histograms_of_phenotypes_and_transformations.pdf'
+  ),
+  width = 7, height = 4 + (length(PHENO_INFO_TABLE$Phenotype) - 1)*1.5
+)
+par(
+  mfrow = c(length(PHENO_INFO_TABLE$Phenotype), 2)
+)
+for(PHENO in PHENO_INFO_TABLE$Phenotype){
+  X <- PLOT_DATA[[PHENO]]
+  XTF <- PLOT_DATA[[PHENO]]
+  TRANSFORM <- PHENO_INFO_TABLE$Transform[PHENO_INFO_TABLE$Phenotype == PHENO]
+
+  if(TRANSFORM != 'none'){
+    XTF <- TRANSFORM_FUNCTIONS[[TRANSFORM]](XTF)
+  }
+  
+  hist(
+    X,
+    main = paste0(
+      PHENO, 
+      ', skewness = ', 
+      signif(PHENO_INFO_TABLE$Skewness[PHENO_INFO_TABLE$Phenotype == PHENO], 3), 
+      ', kurtosis = ', 
+      signif(PHENO_INFO_TABLE$Kurtosis[PHENO_INFO_TABLE$Phenotype == PHENO], 3)
+    ),
+    xlab = NULL,
+    breaks = 100
+  )
+  
+  
+  
+  hist(
+    XTF,
+    main = paste0(
+      PHENO, ', ', 
+      ifelse(
+        TRANSFORM == 'none', 'no transform',
+        paste0(TRANSFORM, ' transform')
+      )
+    ),
+    xlab = NULL,
+    breaks = 100
+  )
+  
+  rm(X, XTF, TRANSFORM)
+}
+dev.off()
+
+rm(PHENO, PLOT_DATA)
+
+#####
+
+
+################################################################################
+##### Prepare phenotype and covariate table #####
+
+DATA <- PHENO_DATA %>% 
+  left_join(
+    ANIMAL_DATA %>% 
+      select(
+        MouseID, Diet, Cohort
+      ),
+    by = 'MouseID'
+  ) %>% 
+  select(
+    MouseID,
+    all_of(PHENO_INFO_TABLE$Phenotype),
+    Age, Timepoint, Diet,
+    DateCollect
+  )
+
+#####
+
+
+################################################################################
+##### Combine into dataset #####
+
+DATASET <- list(
+  Assay = ASSAY,
+  Timepoints = TIMEPOINTS_FOR_ANALYSIS,
+  Info_Table = PHENO_INFO_TABLE,
+  Data = DATA,
+  Transform_Functions = TRANSFORM_FUNCTIONS
+)
+
+#####
+
+
+################################################################################
+#####  #####
+
+#####
+
+
+################################################################################
+##### Output #####
+
+write_csv(
+  PHENO_INFO_TABLE,
+  paste0(
+    'results/', ASSAY,
+    '_phenotype_info_table.csv'
+  )
+)
+
+saveRDS(
+  DATASET,
+  paste0(
+    'data/', ASSAY,
+    '_diet_age_dataset.rds'
+  )
+)
+
+#####
+
+
+################################################################################
+## clear workspace ###########################################
+
+rm(list = ls())
+pacman::p_unload('all')
+graphics.off()
+
+#####
